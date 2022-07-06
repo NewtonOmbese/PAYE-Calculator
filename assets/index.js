93f05825dbd106ebe0372f9c49e8945d217c646c
NewtonOmbese <newtonombese1@gmail.com>
2022-07-06 10:01:33 +0300

    adding global variables

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+(function(){
+
+//global variables
+let getStarted = document.querySelector("#start");
+let monthBox = document.querySelector("#month");
+let yearBox = document.querySelector("#year");
+let yesNSSF = document.querySelector("#yes-nssf");
+let noNSSF = document.querySelector("#no-nssf");
+let yesNHIF = document.querySelector("#yes-nhif");
+let noNHIF = document.querySelector("#no-nhif");
+let newRates = document.querySelector("#new-rates");
+let oldRates = document.querySelector("#old-rates")
+let benefitsInput = document.querySelector("#benefits-input");
+let salaryInput = document.querySelector("#salary-input");
+let calculateBtn = document.querySelector(".calc-btn");
+let period = document.getElementsByName('period');
+
+
+})
