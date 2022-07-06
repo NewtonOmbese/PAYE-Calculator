32a0e2a32934135167421ab6392d13f6d6ebc287
NewtonOmbese <newtonombese1@gmail.com>
2022-07-06 10:03:07 +0300

    adding an event listener

--- assets/index.js
+++ assets/index.js
@@ -15,5 +15,13 @@ let salaryInput = document.querySelector("#salary-input");
 let calculateBtn = document.querySelector(".calc-btn");
 let period = document.getElementsByName('period');
 
+window.addEventListener('DOMContentLoaded',()=>{
+    let salary //= 100000//parseInt(salaryInput.value);
+    let benefits //= 5000//parseInt(benefitsInput.value);
+    let deduct_nssf //= true;
+    let deduct_nhif //= true;
+    let monthly = true;
+    let isNewRates //= true;
+})
 
 })
